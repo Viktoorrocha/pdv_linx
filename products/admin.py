e48e0f928b2f30af4f869ccd71e6c373f08b5152
Viktor Rocha <viktoorrocha@gmail.com>
2025-04-17 19:31:04 -0400

    Finalizando Products

--- products/admin.py
+++ products/admin.py
@@ -3,4 +3,7 @@ from .models import Product
 
 admin.site.register(Product)
 
-
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'is_active', 'created_at', 'updated_at')
+    list_filter = ('is_active', 'created_at')
+    search_fields = ('name,', 'description')
