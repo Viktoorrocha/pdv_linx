5414ee2ec467cf8107150ac2004d7f7614fc9eef
Viktor Rocha <viktoorrocha@gmail.com>
2025-04-18 17:46:29 -0400

    Adicionando imagens aos produtos

--- products/models.py
+++ products/models.py
@@ -9,6 +9,7 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
 
     def __str__(self):
         return self.name
